1f840d209626d9e2e159323eac076bdaf2242310
Srihari <snadendla96@users.noreply.github.com>
2017-05-15 17:27:18 -0700

    Updated a03460000029xdN

--- force-rest-python/apex_rest_get.py
+++ force-rest-python/apex_rest_get.py
@@ -4,7 +4,7 @@ import json
 #get
 if __name__ == "__main__":
     #soql = """Account/0014600000VcqnI"""
-    soql = """Students/a03460000029w9"""
+    soql = """Students/a03460000029xdN"""
     data = util.apexrest(soql)
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(json.loads(data))
